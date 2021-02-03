bc6ff7a595d8d6d4812fce84894321090dd250dc
gugun09 <nandakanaksasak@gmail.com>
2021-02-02 20:59:30 -0800

    Mikrotik-Chr

---
+++ mikrotik-chr.sh
@@ -0,0 +1,14 @@
+wget https://download2.mikrotik.com/routeros/6.38.3/chr-6.38.3.img.zip -O chr.img.zip  && \
+gunzip -c chr.img.zip > chr.img  && \
+mount -o loop,offset=33554944 chr.img /mnt && \
+ADDRESS=`ip addr show ens3 | grep global | cut -d' ' -f 6 | head -n 1` && \
+GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
+echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
+/ip route add gateway=$GATEWAY
+ " > /mnt/rw/autorun.scr && \
+umount /mnt && \
+echo u > /proc/sysrq-trigger && \
+dd if=chr.img bs=1024 of=/dev/vda
+
+chmod +x mikrotik-chr.sh && \
+./mikrotik-chr.sh
